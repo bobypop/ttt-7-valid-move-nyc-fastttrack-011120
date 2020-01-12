1a98b6d03bc86bc21d1e490192e806ef796ea8ac
alex chee <alexchee95@gmail.com>
2020-01-12 16:40:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,7 +14,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(board, input)
-  if board[input] == "X" or board[input] = "O"
+  if board[input] == "X" || board[input] = "O"
     TRUE
   else FALSE
   end
